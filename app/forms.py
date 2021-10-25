bf9637dd31766a1d4cd9112df0adb003af1a1202
Omar Choudhry <71817542+omariosc@users.noreply.github.com>
2021-10-25 21:54:41 +0100

    Implemented create assessment form Title, code, deadline, description fields Set simple only data required validator All string fields

--- app/forms.py
+++ app/forms.py
@@ -1,1 +1,9 @@
-          
+from flask_wtf import Form
+from wtforms.fields.core import DateField, StringField
+from wtforms.validators import DataRequired
+
+class CreateAssessment(Form):
+    title = StringField('title', validators=[DataRequired()])
+    code = StringField('code', validators=[DataRequired()])
+    deadline = StringField('deadline', validators=[DataRequired()])
+    description = StringField('description', validators=[DataRequired()])
