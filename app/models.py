ebe20a096141380c099ad176cfcc2254476199ef
Omar Choudhry <71817542+omariosc@users.noreply.github.com>
2021-10-26 01:23:29 +0100

    Deadline column now Date type Added status column of type Boolean

--- app/models.py
+++ app/models.py
@@ -5,6 +5,7 @@ class Assessment(db.Model):
   id = db.Column(db.Integer, primary_key=True)
   title = db.Column(db.String(100))
   code = db.Column(db.String(10))
-  deadline = db.Column(db.String(10))
+  deadline = db.Column(db.Date())
   description = db.Column(db.String(500))
+  status = db.Column(db.Boolean())
   
