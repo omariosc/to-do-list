1e29d54041f4f75984bedff54ded8d208124b578
Omar Choudhry <71817542+omariosc@users.noreply.github.com>
2021-10-26 01:24:57 +0100

    Connected routes to database Displays all, completed, uncompleted assessments Included description if no tasks exist Adds task to database when creating assessment

--- app/views.py
+++ app/views.py
@@ -6,22 +6,40 @@ import datetime
 
 @app.route('/', methods=['GET', 'POST'])
 def home():
-	home={'description':'All Assessments'}
-	return render_template('home.html',title='All Assessments',home=home)
+	description = 'You have no assessments.'
+	tasks = models.Assessment.query.order_by(models.Assessment.id).all()
+	if len(tasks) > 0 :
+		description = ''
+	for task in tasks:
+		if task.status == 0:
+			task.status = "Uncomplete"
+		elif task.status == 1:
+			task.status = "Completed"
+	return render_template('home.html',title='All Assessments',description=description,tasks=tasks)
 @app.route('/create_assessment', methods=['GET', 'POST'])
 def create():
 	form = CreateAssessment()
 	if form.validate_on_submit():
 		flash('Successfully created assessment.')
-		task = models.Assessment(title=form.title.data,code=form.code.data,deadline=form.deadline.data,description=form.description.data)
+		task = models.Assessment(title=form.title.data,code=form.code.data,deadline=form.deadline.data,description=form.description.data, status=0)
 		db.session.add(task)
-		db.session.commit() 
+		db.session.commit()
 	return render_template('create.html',title='Create Assessments',form=form)
 @app.route('/complete_assessment', methods=['GET', 'POST'])
 def complete():
-	complete={'description':'Completed Assessments'}
-	return render_template('complete.html',title='Completed Assessments',complete=complete)
+	description = 'You have not completed any assessments.'
+	tasks = models.Assessment.query.filter_by(status=1).order_by(models.Assessment.id).all()
+	if len(tasks) > 0 :
+		description = ''
+	for task in tasks:
+		task.status = "Completed"
+	return render_template('complete.html',title='Completed Assessments',description=description,tasks=tasks)
 @app.route('/uncomplete_assessment', methods=['GET', 'POST'])
 def uncomplete():
-	uncomplete={'description':'Uncompleted Assessments'}
-	return render_template('uncomplete.html',title='Uncompleted Asessments',uncomplete=uncomplete)
+	description = 'Well Done! You have no uncompleted assessments.'
+	tasks = models.Assessment.query.filter_by(status=0).order_by(models.Assessment.id).all()
+	if len(tasks) > 0 :
+		description = ''
+	for task in tasks:
+		task.status = "Uncomplete"
+	return render_template('uncomplete.html',title='Uncompleted Asessments',description=description,tasks=tasks)
