faa236ce53679bee0bed1b565bcd860859c890c2
Omar Choudhry <71817542+omariosc@users.noreply.github.com>
2021-10-26 01:23:09 +0100

    Formats deadline to data format d-m-Y

--- app/forms.py
+++ app/forms.py
@@ -1,11 +1,11 @@
-import datetime
+from datetime import date
 from flask_wtf import Form
 from wtforms.fields.core import DateField, StringField
 from wtforms.fields.simple import TextAreaField
-from wtforms.validators import DataRequired, Length
+from wtforms.validators import DataRequired, Length, ValidationError
 
 class CreateAssessment(Form):
     title = StringField('title', validators=[DataRequired(), Length(max=100)])
     code = StringField('code', validators=[DataRequired(), Length(max=10)])
-    deadline = StringField('deadline', validators=[DataRequired(), Length(max=10)])
+    deadline = DateField('deadline', format='%d-%m-%Y', validators=[DataRequired()])
     description = TextAreaField('description', validators=[DataRequired(), Length(max=500)])
