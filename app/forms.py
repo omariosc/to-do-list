490489ab7dbcc60435f26e6110c18ca46434ea53
Omar Choudhry <71817542+omariosc@users.noreply.github.com>
2021-10-27 01:45:29 +0100

    Removed format from deadline Removed non used imports

--- app/forms.py
+++ app/forms.py
@@ -1,11 +1,11 @@
-from datetime import date
 from flask_wtf import Form
-from wtforms.fields.core import DateField, StringField
+from wtforms.fields.core import StringField
+from wtforms.fields.html5 import DateField
 from wtforms.fields.simple import TextAreaField
-from wtforms.validators import DataRequired, Length, ValidationError
+from wtforms.validators import DataRequired, Length
 
 class CreateAssessment(Form):
     title = StringField('title', validators=[DataRequired(), Length(max=100)])
     code = StringField('code', validators=[DataRequired(), Length(max=10)])
-    deadline = DateField('deadline', format='%d-%m-%Y', validators=[DataRequired()])
+    deadline = DateField('deadline')
     description = TextAreaField('description', validators=[DataRequired(), Length(max=500)])
